b13c3cca2c04d7f2a4341e62b9104ec294500774
Aligator000 <justyna.idzi@gmail.com>
2020-04-02 21:56:16 +0200

    refactor: new solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
 
-:(
+const brackets = [];
+var res = str.split(" ");
 
+res.forEach((bracket1) => bracketsConfig.forEach((bracket2) => {
+    if(bracket1 === bracket2) {
+        brackets.push(bracket1);
+    } 
+}));
+    return brackets;
 }
